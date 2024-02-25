639fd636d2c73a74070bc2e4bb58f1f4ff7a5078
Richie <ricia1212@gmail.com>
2024-02-26 00:58:38 +0800

    Change UI

--- src/mocks/navItems.ts
+++ src/mocks/navItems.ts
@@ -2,11 +2,11 @@ import { Asset } from '@/Images'
 
 export const controls = [
   { name: 'Agenda', href: '/', icon: Asset.Calendar },
-  { name: 'Mensagens', href: '/', icon: Asset.Message },
-  { name: 'Relatorios', href: '/', icon: Asset.Diagram }
+  { name: 'Messages', href: '/', icon: Asset.Message },
+  { name: 'Reports', href: '/', icon: Asset.Diagram }
 ]
 
 export const systems = [
-  { name: 'Configurações', href: '/', icon: Asset.Settings },
-  { name: 'Sair', href: '/', icon: Asset.Logout }
+  { name: 'Settings', href: '/', icon: Asset.Settings },
+  { name: 'Sign Out', href: '/', icon: Asset.Logout }
 ]
