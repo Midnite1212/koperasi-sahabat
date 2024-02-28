a359d4b001fa5aca174b0eb476536a538ead2b15
Midnite1212 <richie.salim@bowtie.com.hk>
2024-02-29 01:53:31 +0800

    Prevent crashes between events

--- src/components/EventForm/index.tsx
+++ src/components/EventForm/index.tsx
@@ -5,16 +5,91 @@ import { useEventsContext } from '@/context/EventsContext'
 import { useStateContext } from '@/context/StateContext'
 import { IEvents } from '@/core/types'
 import { EventTypes } from '@/mocks/dataResouces'
+import { useEffect, useMemo, useState } from 'react'
 
 interface IEventForm {
   event?: IEvents
 }
+type TimeRange = {
+  start: Date,
+  end: Date
+}
 
 export const EventForm = ({ event }: IEventForm) => {
-  const { updateEventStorage, removeEventStorage, saveEventStorage } =
+  const { events, updateEventStorage, removeEventStorage, saveEventStorage } =
     useEventsContext()
   const { setModal } = useStateContext()
-  const { register, handleSubmit } = useForm<IEvents>()
+  const { register, handleSubmit, getValues, setValue } = useForm<IEvents>()
+  const formValues = getValues();
+  const [selectedDate, setSelectedDate] = useState('')
+  const [selectedStartTime, setSelectedStartTime] = useState('')
+  const [selectedEndTime, setSelectedEndTime] = useState('')
+  const disabledTimeRanges = useMemo(
+    () =>
+      events.map((event) => {
+        const currEventDate = new Date(event.date)
+        const startTime = event.startDate.split(':')
+        const endTime = event.endDate.split(':')
+        return {
+          start: new Date(
+            currEventDate.getFullYear(),
+            currEventDate.getMonth(),
+            currEventDate.getDate(),
+            Number(startTime[0]),
+            Number(startTime[1])
+          ),
+          end: new Date(
+            currEventDate.getFullYear(),
+            currEventDate.getMonth(),
+            currEventDate.getDate(),
+            Number(endTime[0]),
+            Number(endTime[1])
+          )
+        }
+      }),
+    [events]
+  )
+
+  const isCrahedTime = () => {
+    const startHour = Number(formValues.startDate?.split(':')[0]);
+    const startMinute = Number(formValues.startDate?.split(':')[1]);
+    const endHour = Number(formValues.endDate?.split(':')[0]);
+    const endMinute = Number(formValues.endDate?.split(':')[1]);
+    // Get the disabled time for the selected dates
+    const filterDisabledTimeRanges: TimeRange[] = disabledTimeRanges.filter((timeRange) => {
+      const startDate = timeRange.start
+
+      const disabledDate = new Date(startDate.getFullYear(), startDate.getMonth(), startDate.getDate())
+
+      return disabledDate.toDateString() === new Date(selectedDate).toDateString()
+    })
+    //Translate to time in minutes
+    const startTime = startHour * 60 + startMinute;
+    const endTime = endHour * 60 + endMinute;
+    return filterDisabledTimeRanges.some((timeRange) => {
+      const { start, end } = timeRange
+      const disabledStart = start.getHours() * 60 + start.getMinutes()
+      const disabledEnd = end.getHours() * 60 + end.getMinutes()
+      // console.log(disabledStart, disabledEnd, startTime, endTime)
+      // Case1: start time is between disabled range
+      if (startTime >= disabledStart && startTime <= disabledEnd) return true
+      // Case2: end time is between disabled range
+      if (endTime >= disabledStart && endTime <= disabledEnd) return true
+      // Case3: disabled range is between start and end time
+      if (disabledStart >= startTime && disabledEnd <= endTime) return true
+      // Case4: start and end time is between disabled range
+      if (startTime >= disabledStart && endTime <= disabledEnd) return true
+      return false
+    })
+  }
+
+useEffect(() => {
+  if(isCrahedTime()) {
+    setValue('startDate', '')
+    setValue('endDate', '')
+  }
+},[isCrahedTime, selectedDate, selectedStartTime, selectedEndTime, disabledTimeRanges])
+
   const onSubmit: SubmitHandler<IEvents> = (data) => {
     if (event) {
       updateEventStorage(data)
@@ -57,6 +132,7 @@ export const EventForm = ({ event }: IEventForm) => {
           {...register('date', { required: true })}
           type="date"
           defaultValue={event?.date}
+          onChange={(e) => setSelectedDate(e.target.value)}
           className="mb-2 rounded-lg bg-search p-2 text-sm font-medium text-navTitle"
         />
         <label htmlFor="time" className="text-sm font-medium">
@@ -66,6 +142,7 @@ export const EventForm = ({ event }: IEventForm) => {
           {...register('startDate', { required: true })}
           type="time"
           defaultValue={event?.startDate}
+          onChange={(e) => setSelectedStartTime(e.target.value)}
           className="mb-2 rounded-lg bg-search p-2 text-sm font-medium text-navTitle"
         />
         <label htmlFor="time" className="text-sm font-medium">
@@ -74,7 +151,9 @@ export const EventForm = ({ event }: IEventForm) => {
         <input
           {...register('endDate', { required: true })}
           type="time"
+          onChange={(e) => setSelectedEndTime(e.target.value)}
           defaultValue={event?.endDate}
+
           className="mb-2 rounded-lg bg-search p-2 text-sm font-medium text-navTitle"
         />
         <label htmlFor="type" className="text-sm font-medium">
